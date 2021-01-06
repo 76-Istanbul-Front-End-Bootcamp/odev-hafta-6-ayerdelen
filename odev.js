3bb267aa0d145ac3ed1f657bedc2a01b9eb0aa1f
Aycan Aleyna Yerdelen <aycanyerdelen@gmail.com>
2021-01-06 19:32:46 +0000

    Detail button is upgraded but hw is not done yet.

--- odev.js
+++ odev.js
@@ -14,6 +14,35 @@ window.removePet = (id) => {
     })
 };
 
+window.generateDetailModal = (pet) => {
+
+    return ` <div class="modal fade" id="exampleModal${pet.id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ${pet.name}
+        </div>
+      </div>
+    </div>
+  </div>`
+
+}
+
 window.openPetDetail = (id) => {
-    console.log(id); // show details of the pet with given id
+    fetch(`${window.mockApiUrl}${id}`)
+    .then(response => response.json())
+    .then((data) => {
+      const petModalHTMl =  generateDetailModal(data);
+      document.querySelector("body").innerHTML += petModalHTMl;
+      $(`#exampleModal${id}`).modal('show');
+    })
+
+
+
 };
